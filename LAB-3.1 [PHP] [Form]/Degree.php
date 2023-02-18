d85a590325c3e52a48ed75319eda645a0d9b52bc
Md. Hajjaj Bin Sonosi <90611777+Hajjaj149138@users.noreply.github.com>
2023-02-18 15:43:09 +0600

    Add files via upload

---
+++ LAB-3.1 [PHP] [Form]/Degree.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>
+            Degree
+        </title>
+    </head>
+
+    <body>
+        <form method="POST" action="handler.php">
+            <fieldset>
+            <legend>Degree</legend>
+            <label for="Gender"></label>
+            <input type="checkbox"name="Degree" value="SSC">SSC
+            <input type="checkbox"name="Degree" value="HSC">HSC
+            <input type="checkbox"name="Degree" value="BSc">BSc
+         <hr>
+            
+            <label for="submit"></label>
+            <input type="submit" name="submit" value="submit">
+            </fieldset>
+        </form>
+    </body>
+</html>
---
+++ LAB-3.1 [PHP] [Form]/name.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>
+            Name
+        </title>
+    </head>
+
+    <body>
+        <form method="POST" action="handler.php">
+            <fieldset>
+            <legend>Name</legend>
+            <label for="Name"></label>
+            <input type="text" name="Name" value=""><hr>
+
+            
+            <label for="submit"></label>
+            <input type="submit" name="submit" value="submit">
+            </fieldset>
+        </form>
+    </body>
+</html>
