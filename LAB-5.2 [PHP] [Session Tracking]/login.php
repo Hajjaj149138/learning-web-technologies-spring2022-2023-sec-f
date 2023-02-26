abd6da542ae1b932ca22db44b58bb23ca9ce2017
Md. Hajjaj Bin Sonosi <90611777+Hajjaj149138@users.noreply.github.com>
2023-02-27 02:41:35 +0600

    Add files via upload

---
+++ LAB-5.2 [PHP] [Session Tracking]/login.php
@@ -0,0 +1,105 @@
+<html>
+    <head>
+        <body>
+            <title>Login</title>
+        </body>
+    </head>
+
+    <body>
+        <center>
+            <table border="1" height="550" width="600">
+                <tr height="100">
+                    <td>
+                        <table width="600">
+                            <tr>
+                                <td>
+                                    <h1>X_Company</h1>
+                                </td>
+
+                                <td width="180">
+                                    <a href="publicHome.php">Home</a>|
+                                    <a href="login.php">Login</a>|
+                                    <a href="registration.php">Registration</a>
+                                </td>
+                            </tr>
+                        </table> 
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <center>
+                            <table width="400">
+                                <tr>
+                                    <td>
+                                    <fieldset>
+                                <legend>Login</legend>
+
+                                <table>
+                                    <tr>
+                                        <td width="150">
+                                            <p>User Name</p>
+                                        </td>
+                                        <td>
+                                            <label for="User Name">:</label>
+                                            <input type="text" name="User Name" value="">
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td width="150">
+                                            <p>Password</p>
+                                        </td>
+                                        <td>
+                                            <label for="Password">:</label>
+                                            <input type="password" name="Password" value="">
+                                        </td>
+                                    </tr>
+                                </table><hr>
+
+                                <table width="350">
+                                    <tr>
+                                        <td>
+                                        <label for="Remember Me"></label>
+                                <input type="checkbox" name="Remember Me" value="">Remember Me
+                                        </td>
+                                        <td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            <label for="Submit"></label>
+                                            <input type="submit" name="Submit" value="Submit">
+
+                                            <a href="http://localhost/Lab-5.2%20[PHP]%20Session%20Tracking/LAB-5.2%20[PHP]%20[Session%20Tracking]/forgotPassword.php">Forgot Password?</a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </fieldset>
+                                    </td>
+                                </tr>
+                            </table>
+                        </center>
+                    </td>
+                </tr>
+
+                    <tr height="60">
+                        <td>
+                            <center>
+                                <p>Copyright @ 2017</p>
+                            </center>
+                        </td>
+                    </tr>
+            </table>
+        </center>
+    </body>
+</html>
