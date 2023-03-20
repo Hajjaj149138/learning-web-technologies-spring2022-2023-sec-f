41a9436fc376ca3c158eb75f5074f65735efc0bb
Md. Hajjaj Bin Sonosi <90611777+Hajjaj149138@users.noreply.github.com>
2023-03-20 21:14:21 +0600

    Add files via upload

---
+++ Mid_Lab_Exam/featureManagement.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Feature Management</title>
+    </head>
+
+    <body>
+        <fieldset>
+            <legend><h1>Feature Management</h1></legend>
+
+
+                <ul>
+                    <li><a href="http://localhost/Mid_Lab_Exam/create_feature.php">Create Feature</a></li>
+                    <li><a href="http://localhost/Mid_Lab_Exam/delete_feature.php">Delete Feature</a></li>
+                    <li><a href="http://localhost/Mid_Lab_Exam/update_feature.php">Update Feature</a></li>
+                    <li><a href="http://localhost/Mid_Lab_Exam/view_feature.php">View Features</a></li>
+
+                    <br>
+                    <br>
+                    <br>
+
+                </ul>
+                <a href="http://localhost/Mid_Lab_Exam/login.php">Logout</a>
+
+
+        </fieldset>
+    </body>
+</html>
---
+++ Mid_Lab_Exam/update_feature.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+	<title>Update Feature</title>
+</head>
+<body>
+	<fieldset>
+        <legend><h1>Update Feature</h1></legend>
+        <form action="http://localhost/Mid_Lab_Exam/update_feature.php" method="post">
+		<label for="feature_id">Feature ID:</label>
+		<input type="text" id="feature_id" name="feature_id"><br><br>
+		<label for="new_feature_name">New Feature Name:</label>
+		<input type="text" id="new_feature_name" name="new_feature_name"><br><br>
+		<label for="new_feature_details">New Feature Details:</label><br>
+		<textarea id="new_feature_details" name="new_feature_details"></textarea><br><br>
+		<input type="submit" name="submit" value="Update">
+
+<br><br><br>
+        <a href="http://localhost/Mid_Lab_Exam/featureManagement.php">Feature Management</a>
+	</form>
+    </fieldset>
+</body>
+</html>
+    
+    <?php
+	if (isset($_POST['submit'])) {
+		$feature_id = $_POST['feature_id'];
+		$new_feature_name = $_POST['new_feature_name'];
+		$new_feature_details = $_POST['new_feature_details'];
+
+		$features = file('features.txt');
+		$new_features = array();
+
+		foreach ($features as $feature) {
+			$feature_parts = explode('|', $feature);
+			if ($feature_parts[1] == $feature_id) {
+				$feature_parts[0] = $new_feature_name;
+				$feature_parts[2] = $new_feature_details;
+				$new_features[] = implode('|', $feature_parts);
+			} else {
+				$new_features[] = $feature;
+			}
+		}
+
+		$file = fopen("features.txt", "w");
+		fwrite($file, implode("", $new_features));
+		fclose($file);
+    }
+
